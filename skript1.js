0567e8fbb4d04b9f8de80c24a725206d918df00a
AlyaskaSasha <132011404+AlyaskaSasha@users.noreply.github.com>
2023-04-28 00:34:49 +0300

    Update skript1.js

--- skript1.js
+++ skript1.js
@@ -1,5 +1,7 @@
 "use strict";
-let name = "Sahsa";
-let surname = "Smirnova";
-alert(name);
-alert(surname);
+let str = "!!!";
+alert(str);
+let str2 = "java" , srt22 = "script";
+alert(str2 + str22);
+let str3 = "hello" , str33 = "world";
+alert(str3 + " " + str33);
