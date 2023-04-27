51beb040b5638442ab80430d86c83dc930fc3dc2
AlyaskaSasha <132011404+AlyaskaSasha@users.noreply.github.com>
2023-04-28 00:13:59 +0300

    Update skript1.js

--- skript1.js
+++ skript1.js
@@ -2,5 +2,13 @@
 	многострочный комментарий
 */
 "use strict";
-let a = 1+2+3;
-alert(a);
+let a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+let c = 10;
+let d = 5;
+let result = c + d;
+alert(result);
