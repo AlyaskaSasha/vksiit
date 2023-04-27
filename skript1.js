10f5611d9bea730ffbc7de0fbed756b5cfd36eeb
AlyaskaSasha <132011404+AlyaskaSasha@users.noreply.github.com>
2023-04-28 00:36:29 +0300

    Update skript1.js

--- skript1.js
+++ skript1.js
@@ -1,7 +1,3 @@
 "use strict";
-let str = "!!!";
-alert(str);
-let str2 = "java" , srt22 = "script";
-alert(str2 + str22);
-let str3 = "hello" , str33 = "world";
-alert(str3 + " " + str33);
+let str = 'abcde dklfjdl';
+alert(str.length); 
